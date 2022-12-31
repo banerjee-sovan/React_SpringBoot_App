77c98a6acc1c01cf6143162c509144ae0ddf103f
banerjee-sovan <jgec.sovon93@gmail.com>
2022-12-31 21:31:20 +0530

    commit for initial setup and home page development

---
+++ Frontend/react-library/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+
+export const  App=() =>{
+  return (
+    <div className="App">
+      <div>
+         <Navbar />
+         <HomePage />
+         <Footer />
+      </div>
+      
+    </div>
+  );
+}
+
